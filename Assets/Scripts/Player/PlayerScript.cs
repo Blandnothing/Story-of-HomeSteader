69d1a06285a0c1ce93a2c4cb1d54fa0f518397b0
Bland_nothing <2067721994@qq.com>
2023-06-07 19:06:59 +0800

    优化人物移动

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -8,27 +8,35 @@ public class PlayerScript : MonoBehaviour
 
     [SerializeField] float m_speed = 4.0f;
     [SerializeField] float m_jumpForce = 7.5f;
-    [SerializeField] float m_rollForce = 6.0f;
     [SerializeField] bool m_noBlood = false;
     [SerializeField] Slider sliderHealth;
     [SerializeField] TextMeshProUGUI textHealth;
 
     private Animator m_animator;
     private Rigidbody2D m_body2d;
-    private Sensor_HeroKnight m_groundSensor;
-    private bool m_isWallSliding = false;
-    private bool m_grounded = false;
-    private bool m_rolling = false;
     private int m_facingDirection = 1;
     private int m_currentAttack = 0;
     private float m_timeSinceAttack = 0.0f;
-    private float m_delayToIdle = 0.0f;
-    private float m_rollDuration = 8.0f / 14.0f;
-    private float m_rollCurrentTime;
-
+    //移动
+    public Transform groundCheck;
+    public LayerMask ground;
+    private bool isGround;
     private float inputX;
+    private float graceTimer;
+    [SerializeField] float graceTime;
+    //跳跃
+    public int maxJumpCount = 1;
+    private int jumpCount;
+    private bool jumpPressed;
+    private bool isJump;
+    //生命
     public float maxHealth=100;
     float currentHealth;
+
+    enum stateID
+    {
+
+    }
 
 
     // Use this for initialization
@@ -36,7 +44,6 @@ public class PlayerScript : MonoBehaviour
     {
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
-        m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_HeroKnight>();
         currentHealth = maxHealth;
         textHealth.text = currentHealth.ToString() + "/" + maxHealth.ToString();
     }
@@ -47,52 +54,19 @@ public class PlayerScript : MonoBehaviour
         // Increase timer that controls attack combo
         m_timeSinceAttack += Time.deltaTime;
 
-        // Increase timer that checks roll duration
-        if (m_rolling)
-            m_rollCurrentTime += Time.deltaTime;
-
-        // Disable rolling if timer extends duration
-        if (m_rollCurrentTime > m_rollDuration)
-            m_rolling = false;
-
-        //Check if character just landed on the ground
-        if (!m_grounded && m_groundSensor.State())
-        {
-            m_grounded = true;
-            m_animator.SetBool("Grounded", m_grounded);
-        }
-
-        //Check if character just started falling
-        if (m_grounded && !m_groundSensor.State())
-        {
-            m_grounded = false;
-            m_animator.SetBool("Grounded", m_grounded);
-        }
-
         // -- Handle input and movement --
         inputX = Input.GetAxis("Horizontal");
 
         // Swap direction of sprite depending on walk direction
-        if (inputX > 0)
+        
+
+        if (Input.GetButtonDown("Jump") && jumpCount>0)
         {
-            GetComponent<SpriteRenderer>().flipX = false;
-            m_facingDirection = 1;
+            jumpPressed=true;
         }
+       // --Handle Animations--
 
-        else if (inputX < 0)
-        {
-            GetComponent<SpriteRenderer>().flipX = true;
-            m_facingDirection = -1;
-        }
-
-        if (Input.GetKeyDown(KeyCode.F1))
-        {
-            changeHealth(-10);
-        }
-
-        // -- Handle Animations --
-
-        if (Input.GetMouseButtonDown(0) && m_timeSinceAttack > 0.25f && !m_rolling)
+        if (Input.GetMouseButtonDown(0) && m_timeSinceAttack > 0.25f)
         {
             m_currentAttack++;
 
@@ -110,62 +84,76 @@ public class PlayerScript : MonoBehaviour
             // Reset timer
             m_timeSinceAttack = 0.0f;
         }
-
-        // Block
-        else if (Input.GetMouseButtonDown(1) && !m_rolling)
-        {
-            m_animator.SetTrigger("Block");
-            m_animator.SetBool("IdleBlock", true);
-        }
-
-        else if (Input.GetMouseButtonUp(1))
-            m_animator.SetBool("IdleBlock", false);
-
-        // Roll
-        else if (Input.GetKeyDown("left shift") && !m_rolling && !m_isWallSliding)
-        {
-            m_rolling = true;
-            m_animator.SetTrigger("Roll");
-            m_body2d.velocity = new Vector2(m_facingDirection * m_rollForce, m_body2d.velocity.y);
-        }
-
-
-        //Jump
-        else if (Input.GetKeyDown("space") && m_grounded && !m_rolling)
-        {
-            m_animator.SetTrigger("Jump");
-            m_grounded = false;
-            m_animator.SetBool("Grounded", m_grounded);
-            m_body2d.AddForce(new Vector2(0, m_jumpForce), ForceMode2D.Impulse);
-            m_groundSensor.Disable(0.2f);
-        }
-
-        //Run
-        else if (Mathf.Abs(m_body2d.velocity.x) > Mathf.Epsilon)
-        {
-            // Reset timer
-            m_delayToIdle = 0;
-            m_animator.SetInteger("AnimState", 1);
-        }
-
-        //Idle
-        else
-        {
-            // Prevents flickering transitions to idle
-            m_delayToIdle -= Time.deltaTime;
-            if (m_delayToIdle < 0)
-                m_animator.SetInteger("AnimState", 0);
-        }
     }
     private void FixedUpdate()
     {
-        Debug.Log("当前速度：" + inputX);
-        // Move
-        if (!m_rolling)
-            m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.05f, ground);
+        Debug.Log("isGround:"+isGround);
+        GroundMovement();
+        Jump();
 
-        //Set AirSpeed in animator
-        m_animator.SetFloat("AirSpeedY", m_body2d.velocity.y);
+        SwitchAnim();
+    }
+    void GroundMovement()
+    {
+        m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+        if (inputX > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            m_facingDirection = 1;
+        }
+
+        else if (inputX < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            m_facingDirection = -1;
+        }
+    }
+    void Jump()
+    {
+        if (isGround)
+        {
+            jumpCount = maxJumpCount;
+            isJump = false;
+            graceTimer = graceTime;
+        }
+        else
+        {
+            graceTimer-=Time.fixedDeltaTime;
+        }
+        if(jumpPressed && (isGround  || graceTimer>0))
+        {
+            isJump = true;
+            m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+            graceTimer = 0;
+        }else if(jumpPressed && jumpCount > 0 && isJump)
+        {
+            m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        } 
+    }
+    void SwitchAnim()
+    {
+        m_animator.SetFloat("SpeedX",Mathf.Abs(inputX));
+        if (isGround)
+        {
+            m_animator.SetBool("Falling", false);
+            m_body2d.gravityScale = 1;
+        }        
+        else if (!isGround && m_body2d.velocity.y > 0) {
+            m_animator.SetBool("Jump", true);
+            m_body2d.gravityScale = 1;
+            graceTimer = 0;
+        }    
+        else if (m_body2d.velocity.y<0)
+        {
+            m_animator.SetBool("Jump",false);
+            m_animator.SetBool("Falling", true);
+            m_body2d.gravityScale = 2;
+        }
     }
     public void changeHealth(float amount)
     {
@@ -181,4 +169,5 @@ public class PlayerScript : MonoBehaviour
         }sliderHealth.value = currentHealth/maxHealth;
         textHealth.text=currentHealth.ToString()+"/"+maxHealth.ToString();
     }
+
 }
